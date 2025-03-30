f2b4e5e3da19d234a7639cba3852a338c8223ca4
DiegoMP-maker <diegomedinapoveda@gmail.com>
2025-03-30 12:17:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,8 +150,8 @@ Idioma de corrección: {idioma}
                     "text": consejo_para_audio,
                     "model_id": "eleven_multilingual_v2",
                     "voice_settings": {
-                        "stability": 0.5,
-                        "similarity_boost": 0.8
+                        "stability": 0.3,
+                        "similarity_boost": 0.9
                     }
                 }
                 response_audio = requests.post(url, headers=headers, json=data)
